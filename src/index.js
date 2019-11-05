fbd20470f5cdf78539829779741a1f50eda36c40
Dell <trosko556@gmail.com>
2019-11-05 19:45:57 +0300

    Init todo

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
-import './style.css';
-import printMe from './print.js';
+import "./style.css";
 
-function component() {
-    const wrapper = document.createElement('div');
-    
-    const info = document.createElement('div');
-    const btn = document.createElement('button');
+import {
+    MAIN_BLOCK,
+    INPUT_FIELD,
+    INPUT_BTN_ADD,
+    INPUT_LIST,
+    INPUT_LIST_ID,
+    INPUT_LIST_ITEM_ID,
+    INPUT_LIST_ITEM,
+    INPUT_LIST_ITEM_TEXT,
 
-    info.innerHTML = ['Hello', 'webpack'].join(' ');
-    info.classList.add('hello');
+} from './const.js'
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+const btnAdd = document.querySelector(INPUT_BTN_ADD);
+const inputField = document.querySelector(INPUT_FIELD);
+const mainBlock = document.querySelector(MAIN_BLOCK);
+const inputList = getTemplateNode(INPUT_LIST_ID, INPUT_LIST);
 
-    wrapper.appendChild(btn);
-    wrapper.appendChild(info);
-    return wrapper;
+mainBlock.append(inputList);
+btnAdd.addEventListener("click", addItem);
+inputField.addEventListener("keydown", addItemByKeydown);
+
+function getTemplateNode(id, selector) {
+    return document
+        .querySelector(id)
+        .content.querySelector(selector)
+        .cloneNode(true);
 }
 
-document.body.appendChild(component());
+function addItemByKeydown(eventOfItem) {
+    if (+eventOfItem.keyCode === 13) addItem();
+}
+
+function addItem() {
+    const listItem = getTemplateNode(INPUT_LIST_ITEM_ID, INPUT_LIST_ITEM);
+    listItem.querySelector(INPUT_LIST_ITEM_TEXT).textContent = inputField.value;
+    inputField.value = "";
+    listItem
+        .querySelector(INPUT_LIST_ITEM_BTN_REMOVE)
+        .addEventListener("click", () => listItem.remove());
+    inputList.append(listItem);
+}
